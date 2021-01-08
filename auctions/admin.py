39d12cbb92c7bbe3e122b51510d9c3d45e61a5ab
luisCermeno <luis.cermeno.f@gmail.com>
2021-01-07 23:12:48 -0500

    Added models to models.py and admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+class ListingAdmin(admin.ModelAdmin):
+    filter_horizontal = ("categories",)
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Listing, ListingAdmin)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+    categories = models.ManyToManyField(Category, blank=True, related_name="listings")
+    image = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
