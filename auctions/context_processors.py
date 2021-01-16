a321f125fcedf5afcaaa6909b8b3deacfd6ff627
luisCermeno <luis.cermeno.f@gmail.com>
2021-01-16 11:55:31 -0500

    Final comments

--- auctions/context_processors.py
+++ auctions/context_processors.py
@@ -1,3 +1,7 @@
+# The context processor file describes variables used globally for the 
+# whole web application, in this case its used to have the categories list
+# available in the layout.html file so that it can be included in the navbar
+
 from .models import *
 
 def get_categories(request):
--- auctions/views.py
+++ auctions/views.py
@@ -24,7 +24,6 @@ def category(request, name):
 
 def login_view(request):
     if request.method == "POST":
-
         # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
@@ -75,44 +74,68 @@ def logout_view(request):
 @login_required
 def listing(request, title):
     if request.method == 'POST':
+        # Get the object for that listing
         target_listing = Listing.objects.get(title=title)
+
+        # Determine which action the user is intending to do
         if request.POST['action'] == 'Add to Watchlist':
             try:
+                # Get the watchlist object for that user
                 target_watchlist = WatchList.objects.get(user = request.user)
+                # Add the listing to that watchlist 
                 target_watchlist.listings.add(target_listing)
             except WatchList.DoesNotExist:
+                # If the watchlist does not exist yet, create it
                 new_watchlist = WatchList(user = request.user)
                 new_watchlist.save()
+                # Add the listing to that watchlist 
                 new_watchlist.listings.add(target_listing)
+
         elif request.POST['action'] == 'Delete from Watchlist':
             try:
+                # Get the watchlist object for that user
                 target_watchlist = WatchList.objects.get(user = request.user)
+                # Remove the listing to that watchlist 
                 target_watchlist.listings.remove(target_listing)
             except WatchList.DoesNotExist:
+                # If the watchlist does not exist yet, display an error
                 return HttpResponseBadRequest("Bad Request: watchlist does not exist")
+
         elif request.POST['action'] == 'Comment':
+            # Create a new comment object and save it.
             new_comment = Comment(listing = target_listing, user = request.user, body = request.POST['body'], date = datetime.datetime.now())
             new_comment.save()
+
         elif request.POST['action'] == 'Bid':
+            # Get the bid previous to the new bid made by the user
             previous_bid = Bid.objects.get(listing = target_listing, is_current = True)
+            # Set its current field to false and save it
             previous_bid.is_current = False
             previous_bid.save()
+            # Create new bid with current field set to true and save it
             new_bid = Bid(listing = target_listing, user = request.user, value = float(request.POST['bid']), date = datetime.datetime.now(), is_current=True)
             new_bid.save()
+
         elif request.POST['action'] == 'Close Auction':
+            # Set the listing's close field to True
             target_listing.closed = True
+            # Set the listing's winner field to the user who made the latest bid, and save
             target_listing.winner = Bid.objects.get(listing = target_listing, is_current = True).user
             target_listing.save()
         return HttpResponseRedirect(reverse("listing", args=(title,)))
     else:
         try:
+            # Get listing, comment, bid objects and count the number of bid objects which is_starting field
+            # is set to False
             listing = Listing.objects.get(title=title)
             comments = Comment.objects.filter(listing=listing)
             current_bid = Bid.objects.get(listing = listing, is_current = True)
             nBids = Bid.objects.filter(listing = listing , is_starting = False).count()
             try:
+                # Try to get the watchlist for that user
                 watchlist = WatchList.objects.get(user = request.user).listings.all()
             except:
+                # If the user does not have a watchlist, set it to an empty list
                 watchlist = []
             return render(request, "auctions/listing.html", {
                 "listing": listing,
@@ -129,10 +152,13 @@ def listing(request, title):
 @login_required
 def create(request):
     if request.method == 'POST':
-        category = Category.objects.get(name = request.POST['category'] )
+        # Create a new Listing object
         new_listing = Listing(title = request.POST['title'] , user = request.user, description = request.POST['description'], image = request.POST['image'])
         new_listing.save()
+        # Get the category the user selected and add it to the categories field of the newly created listing
+        category = Category.objects.get(name = request.POST['category'] )
         new_listing.categories.add(category)
+        # Create a new bid with is_starting field set to true which indicates it is the starting bid set by the user
         new_bid = Bid(listing = new_listing, user = request.user, value = float(request.POST['starting_bid']), date = datetime.datetime.now(), is_starting=True, is_current=True)
         new_bid.save()
         return HttpResponseRedirect(reverse("index"))
@@ -144,8 +170,10 @@ def create(request):
 @login_required
 def watchlist(request):
     try:
+        # Try to get the watchlist for that user
         watchlist = WatchList.objects.get(user = request.user).listings.all()
     except:
+        # If the user does not have a watchlist, set it to an empty list
         watchlist = []
     return render(request, "auctions/watchlist.html", {
         "watchlist": watchlist,
@@ -155,8 +183,10 @@ def watchlist(request):
 @login_required
 def myListings(request):
     try:
+        # Try to get the listings which user field is the user who made the request
         listings = Listing.objects.filter(user = request.user)
     except:
+         # If there are no matches, set it to an empty list
         listings = []
     return render(request, "auctions/myListings.html", {
         "listings": listings,
