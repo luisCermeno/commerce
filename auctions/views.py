fe67669e7b3711ffeeea6321b99aeee852919e55
luisCermeno <luis.cermeno.f@gmail.com>
2023-01-18 19:14:06 -0800

    add decorator import

--- auctions/views.py
+++ auctions/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
+from django.views.decorators.csrf import csrf_exempt
 from .models import *
 import datetime
 
